9824cb9f06e4d2450223115ef7f20703c1730838
nainsworth <nja8161995@gmail.com>
2023-10-03 18:57:25 -0500

    created URLs

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request):
+    return HttpResponse('This is  our project page')
+
+def project(request, pk):
+    return HttpResponse('Project Page: ' + str(pk))
