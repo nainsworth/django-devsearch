3cac3a4f0efa8592c1aa8fd7f95e48a714c594e5
nainsworth <nja8161995@gmail.com>
2023-10-04 10:52:51 -0500

    Replaced name and age with simple table

--- projects/views.py
+++ projects/views.py
@@ -6,25 +6,28 @@ projectList = [
         "id": "1",
         "title": "E-Commerce Website",
         "description": "Fully functional e-commerce website",
+        "topRated": True,
     },
     {
         "id": "2",
         "title": "Portfolio Website",
         "description": "A personal website to write articles and display work",
+        "topRated": False,
     },
     {
         "id": "3",
         "title": "Social Website",
         "description": "An open source project built by the community",
+        "topRated": True,
     },
 ]
 
 
 def projects(request):
-    name = "Nicholas Ainsworth"
-    age = 28
+    # name = "Nicholas Ainsworth"
+    # age = 28
 
-    context = {"name": name, "age": age}
+    context = {'projects': projectList}
     return render(request, "projects/projects.html", context)
 
 
