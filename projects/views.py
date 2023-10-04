dfb2c62d655e0f39677de03ae1c0305effa8e943
nainsworth <nja8161995@gmail.com>
2023-10-03 21:01:26 -0500

    Directed URLs to the main template

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('This is  our project page')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('Project Page: ' + str(pk))
+    return render(request, 'projects/single-project.html')
