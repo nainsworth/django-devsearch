8652d577778b629b53acfc602f559ee519709279
nainsworth <nja8161995@gmail.com>
2023-10-03 21:21:25 -0500

    Follow basic Jinja example

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+    {
+        "id": "1",
+        "title": "E-Commerce Website",
+        "description": "Fully functional e-commerce website",
+    },
+    {
+        "id": "2",
+        "title": "Portfolio Website",
+        "description": "A personal website to write articles and display work",
+    },
+    {
+        "id": "3",
+        "title": "Social Website",
+        "description": "An open source project built by the community",
+    },
+]
+
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    name = "Nicholas Ainsworth"
+    age = 28
+
+    context = {"name": name, "age": age}
+    return render(request, "projects/projects.html", context)
+
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    return render(request, "projects/single-project.html")
