95ba12cc0e74d7870b9689b45096bcb82b806774
nainsworth <nja8161995@gmail.com>
2023-10-04 20:25:55 -0500

    Created Review class and migrated db

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+
 class Project(models.Model):
     # owner =
     title = models.CharField(max_length=200)
@@ -10,6 +11,7 @@ class Project(models.Model):
     source_link = models.CharField(max_length=1000, null=True, blank=True)
     vote_total = models.IntegerField(default=0)
     vote_ration = models.IntegerField(default=0)
+    # tags = 
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
@@ -17,3 +19,25 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("up", "up"),
+        ("down", "down"),
+    )
+
+    # owner =
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True, blank=True
+    )
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return self.value
