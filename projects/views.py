8d3a1053d18dbd97e778f9aead4ce56de5375c1a
nainsworth <nja8161995@gmail.com>
2023-10-09 20:15:10 -0500

    created model form and added a new project-form page

--- projects/views.py
+++ projects/views.py
@@ -1,34 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-projectList = [
-    {
-        "id": "1",
-        "title": "E-Commerce Website",
-        "description": "Fully functional e-commerce website",
-        "topRated": True,
-    },
-    {
-        "id": "2",
-        "title": "Portfolio Website",
-        "description": "A personal website to write articles and display work",
-        "topRated": False,
-    },
-    {
-        "id": "3",
-        "title": "Social Website",
-        "description": "An open source project built by the community",
-        "topRated": True,
-    },
-]
-
+from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
     return render(request, "projects/projects.html", context)
-
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
@@ -36,3 +14,8 @@ def project(request, pk):
     reviews = projectObj.review_set.all()
     context = {"project": projectObj, "tags": tags, "reviews": reviews}
     return render(request, "projects/single-project.html", context)
+
+def createProject(request):
+    form = ProjectForm()
+    context = {"form": form}
+    return render(request, 'projects/project-form.html', context)
