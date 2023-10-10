57bf28c5f9d3a744b66de7c7d53cb9595a0f0c72
nainsworth <nja8161995@gmail.com>
2023-10-09 20:24:49 -0500

    DB Changes form the Ratio spelling correction in models.py

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
@@ -17,5 +17,11 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    
     context = {"form": form}
     return render(request, 'projects/project-form.html', context)
