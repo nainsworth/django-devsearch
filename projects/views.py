773b205e5ac5b398642093e46a68eaa985991160
nainsworth <nja8161995@gmail.com>
2023-10-09 20:46:17 -0500

    organized views.py

--- projects/views.py
+++ projects/views.py
@@ -14,25 +14,32 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
     reviews = projectObj.review_set.all()
+    template = "projects/single-project.html"
+    
     context = {"project": projectObj, "tags": tags, "reviews": reviews}
-    return render(request, "projects/single-project.html", context)
+    return render(request, template, context)
 
 
 def createProject(request):
+    context = {}
     form = ProjectForm()
+    template = "projects/project-form.html"
+
     if request.method == "POST":
         form = ProjectForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("projects")
 
-    context = {"form": form}
-    return render(request, "projects/project-form.html", context)
+    context["form"] = form
+    return render(request, template, context)
 
 
 def updateProject(request, pk):
+    context = {}
     projectObj = Project.objects.get(id=pk)
     form = ProjectForm(instance=projectObj)
+    template = "projects/project-form.html"
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, instance=projectObj)
@@ -40,5 +47,5 @@ def updateProject(request, pk):
             form.save()
             return redirect("projects")
 
-    context = {"form": form}
-    return render(request, "projects/project-form.html", context)
+    context['form'] = form
+    return render(request, template, context)
