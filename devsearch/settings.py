0ae73ae56ee7bf5d7ec8f211d8ddbd2bd8ae41e2
nainsworth <nja8161995@gmail.com>
2023-10-10 20:12:47 -0500

    Added defaults for project Images

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#t1ewau8$3)irzh!qa%m$5r^%+=t56p(qm^yaxr757&l-3md=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
