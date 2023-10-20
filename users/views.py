3184c6fab2e8095c57de8d06cb3c0c3fd28a2e27
nainsworth <nja8161995@gmail.com>
2023-10-19 21:11:02 -0500

    Created user profile page and user profile views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
 
 def profiles(request):
     template = "users/profiles.html"
+    profiles = Profile.objects.all()
 
-    return render(request, template)
+    context = {'profiles': profiles}
+    return render(request, template, context)
+
+def userProfiles(request, pk):
+    profileObj = Profile.objects.get(id=pk)
+    topSkills = profileObj.skill_set.exclude(description__exact="")
+    otherSkills = profileObj.skill_set.filter(description="")
+    template = "users/user-profile.html"
+
+    context = {'profile': profileObj, 'topSkills': topSkills, 'otherSkills': otherSkills}
+    return render(request, template, context)
