032de0f813c52f68e83230832d458d825b558acc
nainsworth <nja8161995@gmail.com>
2023-12-05 21:11:57 -0600

    Create a Messaging system to message users

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 import uuid
 
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -44,3 +43,30 @@ class Skill(models.Model):
     def __str__(self):
         return str(self.name)
 
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    recipient = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="messages",
+    )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return str(self.subject)
+
+    class Meta:
+        ordering = ["is_read", "-created"]
