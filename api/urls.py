8e084333eb7596607d352d6fbc2f883f0837dd61
nainsworth <nja8161995@gmail.com>
2023-12-10 20:54:09 -0600

    Integrating postman API

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    # Tokens
+    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Pages
     path("", views.getRoutes),
     path("projects/", views.getProjects),
-    path("projects/<str:pk>", views.getProject),
+    path("projects/<str:pk>/", views.getProject),
+    path("projects/<str:pk>/vote/", views.projectVote),
 ]
